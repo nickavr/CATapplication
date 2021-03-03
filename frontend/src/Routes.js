df2ff2453b90be283e8e2d7f5d3aef8d01a102fd
nickavr <nicuavram11@gmail.com>
2021-03-03 11:14:51 +0200

    home page and google auth

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './containers/LandingPage/LandingPage';
+import HomePage from './containers/admin/HomePage/HomePage';
 
 export default function Routes() {
     return (
         <Router>
             <Switch>
                 <Route path="/" exact component={LandingPage}></Route>
+                <Route path="/home" exact component={HomePage}></Route>
             </Switch>
         </Router>
     );
---
+++ frontend/src/containers/admin/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import './HomePage.css';
+import React from 'react';
+
+const HomePage = function () {
+    return <h1>HomePage</h1>;
+};
+
+export default HomePage;
